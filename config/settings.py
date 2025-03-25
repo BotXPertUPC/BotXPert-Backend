313475a0e5287e149e523cf23bd0fadb1f4d2704
Anyer <anyer.moreno@estudiantat.upc.edu>
2025-03-25 21:18:58 +0100

    Canvi per utilitzar la entorn de variable .env.prod

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
+load_dotenv(dotenv_path=".env.prod")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
