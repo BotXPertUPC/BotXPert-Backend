127d53864d3cc2225845ed75075193a619f3a132
Anyer <anyer.moreno@estudiantat.upc.edu>
2025-04-24 20:38:34 +0200

    Creació dels serializers

---
+++ botflow/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import BotFlow, Node, ListOption
+
+
+class ListOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ListOption
+        fields = ['id', 'label', 'target_node', 'node']
+
+
+class NodeSerializer(serializers.ModelSerializer):
+    list_options = ListOptionSerializer(many=True, read_only=True)
+    next_node = serializers.PrimaryKeyRelatedField(queryset=Node.objects.all(), allow_null=True, required=False)
+    id = serializers.IntegerField(required=True)
+
+    class Meta:
+        model = Node
+        fields = [
+            'id', 'bot_flow', 'type', 'text', 'position_x', 'position_y',
+            'list_header', 'next_node', 'list_options'
+        ]
+
+
+class BotFlowSerializer(serializers.ModelSerializer):
+    nodes = NodeSerializer(many=True, read_only=True)
+    start_node = serializers.PrimaryKeyRelatedField(queryset=Node.objects.all(), allow_null=True, required=False)
+
+    class Meta:
+        model = BotFlow
+        fields = [
+            'id', 'name', 'description', 'created_at', 'updated_at',
+            'phone_number', 'start_node', 'nodes'
+        ]
