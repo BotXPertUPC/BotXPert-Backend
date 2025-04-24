d94c649d6c5d5e63f047e85361970b17da709f88
Anyer <anyer.moreno@estudiantat.upc.edu>
2025-04-24 21:14:52 +0200

    Get personalitzar per obtenir tots els nodes d'un botflow.

--- botflow/views.py
+++ botflow/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import BotFlow, Node, ListOption
 from .serializers import BotFlowSerializer, NodeSerializer, ListOptionSerializer
 
@@ -16,3 +16,10 @@ class NodeViewSet(viewsets.ModelViewSet):
 class ListOptionViewSet(viewsets.ModelViewSet):
     queryset = ListOption.objects.all()
     serializer_class = ListOptionSerializer
+
+class NodesByBotFlowView(generics.ListAPIView):
+    serializer_class = NodeSerializer
+
+    def get_queryset(self):
+        botflow_id = self.kwargs['botflow_id']
+        return Node.objects.filter(bot_flow_id=botflow_id)
